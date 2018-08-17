c07c3c54214d74be730a5fb5691226203cf2b376
rgajdov <rgajdov@gmail.com>
2018-08-17 16:38:57 +0300

    Remove files from repo

--- lesson1/src/main/java/Apple.java
+++
@@ -1,6 +0,0 @@
-public class Apple extends Fruit {
-
-    public Apple() {
-        this.weight = 1.0f;
-    }
-}
--- lesson1/src/main/java/Fruit.java
+++
@@ -1,8 +0,0 @@
-public class Fruit {
-
-    float weight;
-
-    public float getWeight() {
-        return weight;
-    }
-}
